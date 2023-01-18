43f25a445e45da82a8061998382a175cee06e966
Özgür Yildirim <ozguryenihayat@gmail.com>
2023-01-18 01:22:27 +0100

    Implement ^ and $

--- main.go
+++ main.go
@@ -1,48 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
 const separator = "|"
 
 func main() {
-	pattern, testString := getInput()
-	fmt.Println(match(pattern, testString))
+	pattern, test := getInput()
+	fmt.Println(match(pattern, test))
 }
 
 func getInput() (string, string) {
-	var input string
-	fmt.Scanln(&input)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	if strings.Count(input, separator) != 1 {
+		log.Fatal("Input must contain one '|' as separator")
+	}
 	splitResult := strings.SplitN(input, separator, 2)
 	return splitResult[0], splitResult[1]
 }
 
 func match(pattern, test string) bool {
-	patternLength := len(pattern)
-	if len(test) < patternLength {
-		return false
-	}
-	if matchEqual(pattern, test[:patternLength]) {
+	pLen, tLen := len(pattern), len(test)
+	if pLen == 0 || (pattern == "^$" && tLen == 0) || pattern == "^" || pattern == "$" {
 		return true
+	} else if tLen == 0 || pattern == "^$" {
+		return false
+	} else if strings.HasPrefix(pattern, "^") && strings.HasSuffix(pattern, "$") {
+		return matchExact(pattern[1:pLen-1], test)
+	} else if strings.HasPrefix(pattern, "^") {
+		return matchExact(pattern[1:], test[:pLen-1])
+	} else if strings.HasSuffix(pattern, "$") {
+		return matchExact(pattern[:pLen-1], test[tLen-pLen+1:])
+	} else {
+		return matchFlex(pattern, test)
 	}
-	return match(pattern, test[1:])
 }
 
-func matchEqual(regex, test string) bool {
-	if regex == "" {
-		return true
-	}
-	if test == "" {
-		return false
-	}
-	if matchOne(regex[:1], test[:1]) {
-		return matchEqual(regex[1:], test[1:])
-	}
-	return false
+func matchFlex(pattern, test string) bool {
+	pLen, tLen := len(pattern), len(test)
+	return pLen <= tLen && (matchExact(pattern, test[:pLen]) || matchFlex(pattern, test[1:]))
 }
 
-func matchOne(regex, test string) bool {
-	return regex == "." || regex == test
+func matchExact(pattern, test string) bool {
+	pLen, tLen := len(pattern), len(test)
+	return pLen == 0 || pLen == tLen && matchOne(pattern[:1], test[:1]) && matchExact(pattern[1:], test[1:])
+}
+
+func matchOne(pattern, test string) bool {
+	return pattern == "." || pattern == test
 }
